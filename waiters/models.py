ac7d75973419da3d73ce136f0df06664a21796cf
Ayano <s.correa.wilches@gmail.com>
2023-06-15 21:55:22 -0500

    add waiters app & model

---
+++ waiters/models.py
@@ -0,0 +1,13 @@
+"""_summary_
+"""
+from django.db import models
+
+# Create your models here.
+
+
+class Waiter(models.Model):
+    """_summary_
+    """
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
